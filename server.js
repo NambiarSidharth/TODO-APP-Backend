854822dbfdc027b2e5ebf3dbcefd0553699298ff
NambiarSidharth <32272949+NambiarSidharth@users.noreply.github.com>
2019-05-12 10:59:15 +0530

    some change

--- server.js
+++ server.js
@@ -16,8 +16,6 @@ mongoose
 app.get("/",(req,res)=>{
     res.status(200).json({message:"working"})
 })
-    
-
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
       console.log("app listening on port:" + port);
